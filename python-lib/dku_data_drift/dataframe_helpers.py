720f749a2f04339dc825b2ce2d9d1b6cca33d720
Thibault Desfontaines <desfontainesthibault@gmail.com>
2020-03-16 16:33:52 +0100

    Add some docstrings.

--- python-lib/dku_data_drift/dataframe_helpers.py
+++ python-lib/dku_data_drift/dataframe_helpers.py
@@ -1,7 +1,15 @@
-# coding: utf-8
+"""
+    Simple functions helpers
+"""
+
+import logging
 import numpy as np
+import sys
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='Tableau Plugin | %(levelname)s - %(message)s')
 
+logger.info("Python version: {}".format(sys.version))
 # python3 does not have basetring
 try:
     basestring
@@ -10,13 +18,27 @@ except NameError:
 
 
 def not_enough_data(df, min_len=1):
+    """
+        Compare length of dataframe to minimum lenght of the test data.
+        Used in the relevance of the measure.
+    :param df: Input dataframe
+    :param min_len:
+    :return:
+    """
     return len(df) < min_len
+
 
 def nothing_to_do(stuff):
     return stuff is None
 
 
 def generic_check_compute_arguments(datetime_column, groupby_columns):
+    """
+        Check columns argument in the dataframe. Date is always tricky to handle.
+    :param datetime_column:
+    :param groupby_columns:
+    :return:
+    """
     if not isinstance(datetime_column, basestring):
         raise ValueError('datetime_column param must be string. Got: ' + str(datetime_column))
     if groupby_columns:
