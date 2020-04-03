e19968a605da6f4a48f9d0704a7a336b1df98733
Thibault Desfontaines <desfontainesthibault@gmail.com>
2020-04-03 18:33:37 +0200

    Handle object dataset type in the recipes for partition columns

--- python-lib/dku_data_drift/dataset_helpers.py
+++ python-lib/dku_data_drift/dataset_helpers.py
@@ -4,7 +4,7 @@ def get_partitioning_columns(dataset):
     :param dataset:
     :return: The partitioning columns as a list of strings
     """
-    partitioning_dimensions = dataset.get_definition().get('partitioning', {}).get('dimensions', [])
+    partitioning_dimensions = dataset.get_config().get('partitioning', {}).get('dimensions', [])
     if len(partitioning_dimensions) > 0:
         partitioning_type = partitioning_dimensions[0].get('type')
         if partitioning_type == 'value':
