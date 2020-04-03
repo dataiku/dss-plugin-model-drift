c60b36a0aae1c20c9d00eed8b53e4986b1edf2f8
Thibault Desfontaines <desfontainesthibault@gmail.com>
2020-04-03 18:05:37 +0200

    Replace None by []

--- python-lib/dku_data_drift/dataset_helpers.py
+++ python-lib/dku_data_drift/dataset_helpers.py
@@ -2,7 +2,7 @@ def get_partitioning_columns(dataset):
     """
     Dataset DSS object
     :param dataset:
-    :return:
+    :return: The partitioning columns as a list of strings
     """
     partitioning_dimensions = dataset.get_definition().get('partitioning', {}).get('dimensions', [])
     if len(partitioning_dimensions) > 0:
@@ -10,4 +10,4 @@ def get_partitioning_columns(dataset):
         if partitioning_type == 'value':
             partitioning_columns = [col.get('name') for col in partitioning_dimensions]
             return partitioning_columns
-    return None
+    return []
