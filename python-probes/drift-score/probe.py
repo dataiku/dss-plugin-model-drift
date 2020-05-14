8ea647c27305fafddbf14a23008ddd9fed3e5fb3
Du Phan <du.phan@dataiku.com>
2020-05-14 09:11:29 +0200

    replace string by ModelDriftConstants variables, catch edge cases

--- python-probes/drift-score/probe.py
+++ python-probes/drift-score/probe.py
@@ -3,6 +3,11 @@ import numpy as np
 
 def process(dataset, partition_id):
     df = dataset.get_dataframe()
-    most_recent_drift_score = df[df[ModelDriftConstants.TIMESTAMP] == np.max(df['timestamp'])]['drift_score'].values[0]
-    metric_values = {ModelDriftConstants.DRIFT_SCORE: most_recent_drift_score}
-    return metric_values
+    if len(df) == 0:
+        return 'No data'
+    if ModelDriftConstants.DRIFT_SCORE in df and ModelDriftConstants.TIMESTAMP in df:
+        most_recent_drift_score = df[df[ModelDriftConstants.TIMESTAMP] == np.max(df[ModelDriftConstants.TIMESTAMP])][ModelDriftConstants.DRIFT_SCORE].values[0]
+        metric_values = {ModelDriftConstants.DRIFT_SCORE: most_recent_drift_score}
+        return metric_values
+    else:
+        return 'No drift score'
